d2fb4a15673968de876997d895a63de35eb61d95
jess <162760617+craicken@users.noreply.github.com>
2024-03-15 01:02:00 +0000

    change grid square color on mouseover

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ const container = document.querySelector("#container");
 for (let i = 0; i < 256; i++) {
     const div = container.appendChild(document.createElement("div"));
     div.classList.add("grid-square");
+    div.addEventListener("mouseover", () => {
+        div.classList.toggle("grid-active");
+    })
 }

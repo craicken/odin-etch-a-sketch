237c68fb73e9dc49330c68133edf899b01f4befa
jess <162760617+craicken@users.noreply.github.com>
2024-03-15 01:10:22 +0000

    add button to reset grid and colors

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("#new-grid");
 
-for (let i = 0; i < 256; i++) {
-    const div = container.appendChild(document.createElement("div"));
-    div.classList.add("grid-square");
-    div.addEventListener("mouseover", () => {
-        div.classList.toggle("grid-active");
-    })
+
+function setGrid() {
+    for (let i = 0; i < 256; i++) {
+        const div = container.appendChild(document.createElement("div"));
+        div.classList.add("grid-square");
+        div.addEventListener("mouseover", () => {
+            div.classList.toggle("grid-active");
+        })
+    }
 }
+
+function removeGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+button.addEventListener("click", () => {
+    removeGrid();
+    setGrid();
+})
+
+setGrid();
